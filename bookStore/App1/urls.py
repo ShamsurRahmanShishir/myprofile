d4f7c28b090ceba87d5c198e7ae7a6e2b92ac918
shamsurRahmanShishir <md.shamsur.rahman.shishir@g.bracu.ac.bd>
2023-08-20 21:57:29 +0600

    first commit

---
+++ bookStore/App1/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.ind, name='index'),
+    path('cUS/',views.contactUs, name='contact'),
+    path('img/',views.img, name='img'),
+    
+
+
+]
